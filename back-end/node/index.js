20a71425d0822d04681fcd09d2337b7ac58a00e1
amendeztheran <amendeztheran@gmail.com>
2023-10-11 15:19:14 -0500

    Endpoint request index.js
    11 /10/2023, se realiza las peticiones de Endpoint request GET POST PUT DELETE.

--- back-end/node/index.js
+++ back-end/node/index.js
@@ -1,5 +1,12 @@
-import express from "express";
+import express from "express"
 
-const app = express();
+const app = express()
 
-app.listen(4200);
+//Endpoint para consultar desde aplicaciones clientes
+app.get('/employess' , (req, res) => res.send ('obteniendo empleados'))//Leer empleados
+app.post('/employess' , (req, res) => res.send ('creando empleados'))//Crear empleados
+app.put('/employess' , (req, res) => res.send ('actualizando empleados'))//Actualizar empleados
+app.delete('/employess' , (req, res) => res.send ('eliminando empleados'))//Eliminar empleados
+
+app.listen(4200)
+console.log('Corriendo el servidor 4200')
