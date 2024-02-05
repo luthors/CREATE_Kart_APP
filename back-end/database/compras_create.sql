6170d18a7f349428d7b6af0dd99a9a8ef08e46c2
NadiaCiro <luciguci262006@gmail.com>
2024-02-05 11:26:01 -0500

    sql de la base de datos

--- back-end/database/compras_create.sql
+++ back-end/database/compras_create.sql
@@ -404,3 +404,15 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2024-01-31 19:31:09
+
+
+ALTER TABLE `compras_create`.`order_header` 
+ADD COLUMN `correo` VARCHAR(45) NULL AFTER `customer`;
+
+-- usarlo si se creó una columna en orders_detail
+ALTER TABLE `compras_create`.`orders_detail` 
+DROP COLUMN `adress`;
+
+ALTER TABLE `compras_create`.`orders_detail` 
+ADD COLUMN `size` VARCHAR(45) NULL AFTER `total`,
+ADD COLUMN `color` VARCHAR(45) NULL AFTER `size`;
